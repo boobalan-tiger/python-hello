0c8cc19fc6cf937bf2813ac7dfde9b99ddb0977b
boobalan-tiger <boobalan.anbalag@tigeranalytics.com>
2024-08-20 12:49:57 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ import os
 def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
-        name = "worldbb"
-    message = "Hello, " + name + "!\n"
+        namee = "worldbb"
+    message = "Hello, " + namee + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
